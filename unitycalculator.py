bda9ecff316687f9f35cf24b899d563e9d3690b9
Laspimon <casperlehmann@gmail.com>
2015-11-28 14:17:02 +0100

    Doing addition.

--- unitycalculator.py
+++ unitycalculator.py
@@ -5,6 +5,8 @@ import unittest, re
 
 class Calc(object):
 
+    factored = None
+
     def __init__(self):
         self.add = re.compile('\+?')
         self.substract = re.compile('-?')
@@ -18,7 +20,9 @@ class Calc(object):
     def main(self):
         while True:
             inp = raw_input(' >')
-            print (self.parse_input(inp))
+            self.factored = self.parse_input(inp)
+            print (self.factored)
+            break
 
 
     def parse_input(self, inp, factors = []):
@@ -31,6 +35,27 @@ class Calc(object):
                 return factored
         else:
             return inp
+
+    def evaluate_factors(self, focus = None):
+        if focus is None: focus = self.factored
+        return self.evaluate_factor(focus)
+        return [self.evaluate_factor(focus[0]),
+                focus[1],
+                self.evaluate_factor(focus[2])]
+
+    def evaluate_factor(self, focus = None):
+        if isinstance(focus, str):
+            if [ord('0') <= ord(char) <= ord('9') for char in focus]:
+                return int(focus)
+        if isinstance(focus, list):
+            if not isinstance(focus[0], int):
+                focus[0] = self.evaluate_factor(focus[0])
+            if not isinstance(focus[2], int):
+                focus[2] = self.evaluate_factor(focus[2])
+        if focus[1] == '+':
+            if sum([isinstance(_, int) for _ in focus[::2]]) == 2:
+                return sum([int(_) for _ in focus[::2]])
+        return focus
 
 class TestParseInput(unittest.TestCase):
 
@@ -58,6 +83,20 @@ class TestParseInput(unittest.TestCase):
         calc = Calc()
         out = calc.parse_input('4*7+2/4-1')
         self.assertEqual(out, [['4', '*', '7'], '+', [['2', '/', '4'], '-', '1']])
+
+class TestEvaluateFactors(unittest.TestCase):
+
+    def test_evaluate_1_plus_2(self):
+        calc = Calc()
+        calc.factored = calc.parse_input('1+2')
+        out = calc.evaluate_factors()
+        self.assertEqual(out, 3)
+
+    def test_evaluate_1_plus_2_plus_3(self):
+        calc = Calc()
+        calc.factored = calc.parse_input('1+2+3')
+        out = calc.evaluate_factors()
+        self.assertEqual(out, 6)
 
 if __name__ == '__main__':
     unittest.main()
