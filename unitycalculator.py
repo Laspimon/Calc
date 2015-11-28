739518cdcdf71f97c93c570d453fe647eb1a608e
Laspimon <casperlehmann@gmail.com>
2015-11-28 19:51:48 +0100

    Exceptions and testing.

--- unitycalculator.py
+++ unitycalculator.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 #-*-coding:utf8-*-
 from __future__ import print_function
-import unittest, re
+from functools import partial
+
+import unittest, re, argparse
 
 class Calc(object):
 
     def __init__(self):
+        """Make regular expressions for parsing input.
+        """
         self.add = re.compile('\+')
         # Supports division and multiplication with negative numbers: "/-", "*-"
         self.substract = re.compile('(?<!^)(?<!\*)(?<!/)-')
@@ -17,14 +21,25 @@ class Calc(object):
                       '/': self.divide}
 
     def main(self):
+        """Main loop. Start here.
+        """
         while True:
-            inp = ''.join(raw_input(' > ').split(' '))
-            factored = self.parse_input(inp)
-            print ('  ', self.get_presentable_output(
-                self.evaluate_factors(factored)))
-            break
+            try:
+                inp = ''.join(raw_input(' > ').split(' '))
+                factored = self.parse_input(inp)
+            except ValueError as e:
+                print (4*' ' + '\n'.join(e.args))
+                continue
+            except KeyboardInterrupt:
+                print ('\n', 4*' ' + 'bye bye')
+                break
+            else:
+                print ('  ', self.get_presentable_output(
+                        self.evaluate_factors(factored)))
 
     def get_presentable_output(self, evaluated):
+        """Quick method for formatting output.
+        """
         a, b = evaluated
         if b == 1:
             return str(a)
@@ -32,21 +47,34 @@ class Calc(object):
             return '{} / {}'.format(a,b)
 
 
-    def parse_input(self, inp, factors = []):
+    def parse_input(self, inp):
+        """Takes an input string and parses it as a fraction. Returns tuple.
+        """
+        if inp == '':
+            raise ValueError('No input.')
+        if False in [_ in '0123456789-+*/' for _ in inp]:
+            raise ValueError('Integers (0-9) & operators (+-*/) only, please.')
+        if inp[0] in '+*/':
+            raise ValueError('First sign connot be "+", "*" or "/".')
         for f in '+-*/':
             if self.signs[f].search(inp, 1):
                 factored = self.signs[f].split(inp, 1)
-                factored.insert(1, f)
-                factored[0] = self.parse_input(factored[0])
-                factored[2] = self.parse_input(factored[2])
-                return factored
+                a = self.parse_input(factored[0])
+                b = self.parse_input(factored[1])
+                if f == '/' and b == '0':
+                    raise ValueError('Divide by zero. Please don\'t.')
+                return [a, f, b]
         else:
             return inp
 
     def evaluate_factors(self, focus):
+        """Handler for evaluate_factor.
+        """
         return self.reduce(self.evaluate_factor(focus))
 
     def evaluate_factor(self, focus):
+        """Evaluate the parsed input.
+        """
         if isinstance(focus, str):
             if self.is_number(focus):
                 return (int(focus), 1)
@@ -69,6 +97,8 @@ class Calc(object):
             return a[0] * b[0], a[1] * b[1]
 
     def find_common_denominator(self, a, b):
+        """Find the common denominator from two denominators.
+        """
         if a[1]%b[1] == 0:
             return a[1]
         elif b[1]%a[1] == 0:
@@ -77,6 +107,8 @@ class Calc(object):
             return a[1]*b[1]
 
     def reduce(self, fraction):
+        """Reduce the fraction.
+        """
         a, b = fraction
         # Wanting to reduce the fraction, we loop from the denominator and down.
         for guess in range(b, 1, -1):
@@ -88,8 +120,10 @@ class Calc(object):
         return a, b
 
     def is_number(self, string):
+        """\"string\" is a number. May be negative.
+        """
         if string[0] == '-' and len(string)>1: string = string[1:]
-        return not False in [ord('0') <= ord(char) <= ord('9') for char in string]
+        return not False in [ord('0')<=ord(char)<= ord('9') for char in string]
 
 class TestParseInput(unittest.TestCase):
 
@@ -121,7 +155,7 @@ class TestParseInput(unittest.TestCase):
     def test_parse_plus_minus_multiply_divide(self):
         calc = Calc()
         out = calc.parse_input('4*7+2/4-1')
-        self.assertEqual(out, [['4', '*', '7'], '+', [['2', '/', '4'], '-', '1']])
+        self.assertEqual(out, [['4', '*', '7'],'+',[['2', '/', '4'],'-','1']])
 
 class TestEvaluateFactors(unittest.TestCase):
 
@@ -179,9 +213,30 @@ class TestEvaluateFactors(unittest.TestCase):
         out = calc.evaluate_factors(factored)
         self.assertEqual(out, (-1,2))
 
+    def test_evaluate_no_input(self):
+        calc = Calc()
+        self.assertRaises(ValueError,
+                          partial(calc.parse_input, ''))
 
+    def test_evaluate_divide_by_zero(self):
+        calc = Calc()
+        self.assertRaises(ValueError,
+                          partial(calc.parse_input, '1/0'))
+
+    def test_evaluate_divide_nothing_by_1(self):
+        calc = Calc()
+        self.assertRaises(ValueError,
+                          partial(calc.parse_input, '/1'))
 
 if __name__ == '__main__':
-    Calc().main()
-    #unittest.main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run', help='Run the calculator (no testing).',
+                        action='store_true')
+    args = parser.parse_args()
+
+    if args.run:
+        Calc().main()
+    else:
+        unittest.main()
+
 
